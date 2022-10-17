7442ae634e676f503d62f4694221e66d2567a3ec
Daniel Mantovani <dmanto@gmail.com>
2022-10-17 13:18:44 -0300

    add github action workflows, bump to v0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,3 +8,8 @@ First release.
 ## v0.0.2 (2022-10-17)
 
 Add basic test.
+
+## v0.0.3 (2022-10-17)
+
+Add a simple connection, subscribe and publish test.
+Add workflows for github actions CI. (very basic, uses free mqtt://test.mosquitto.org service)
--- src/mojo-plugin-mqtt-helper.ts
+++ src/mojo-plugin-mqtt-helper.ts
@@ -1,19 +1,14 @@
-import type {MojoApp, ConfigOptions} from '@mojojs/core/lib/types';
+import type {MojoApp} from '@mojojs/core/lib/types';
 import type {IClientOptions, AsyncMqttClient} from 'async-mqtt';
 import {connect} from 'async-mqtt';
-interface MqttOptions extends ConfigOptions {
-  brokerUrl?: string;
-  mqttOptions?: IClientOptions;
-}
+
 /**
  * mqttPlugin.
  * @param app
- * @param options
  */
-export default function mqttPlugin(app: MojoApp, options: MqttOptions) {
-  app.addHelper('mqttClient', () => {
-    const brokerUrl = options.brokerUrl ?? 'mqtt://localhost:1883';
-    return connect(brokerUrl, options.mqttOptions);
+export default function mqttPlugin(app: MojoApp) {
+  app.addHelper('mqttClient', (_ctx, brokerUrl: any, opts: IClientOptions) => {
+    return connect(brokerUrl, opts);
   });
 }
 
