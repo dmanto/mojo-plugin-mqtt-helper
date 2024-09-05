c53a6382db3a2a5cd5c4f7ea703cf268055d29bf
Daniel Mantovani <dmanto@gmail.com>
2024-09-05 15:35:09 -0300

    improve several typescript declarations

--- src/mojo-plugin-mqtt-helper.ts
+++ src/mojo-plugin-mqtt-helper.ts
@@ -1,18 +1,19 @@
 import type {MojoApp} from '@mojojs/core/lib/types';
-import {connectAsync, type MqttClient, type IClientOptions} from 'mqtt';
+import {connectAsync, type MqttClient} from 'mqtt';
 
 /**
  * mqttPlugin.
  * @param app
  */
 export default function mqttPlugin(app: MojoApp) {
-  app.addHelper('mqttClient', (_ctx, brokerUrl?, opts?, allowRetries?) => {
-    return connectAsync(brokerUrl !== undefined ? brokerUrl : 'mqtt://localhost:1833', opts, allowRetries);
+  app.addHelper('mqttClient', (_ctx, ...args: Parameters<typeof connectAsync>) => {
+    const [brokerUrl, ...rest] = args;
+    return connectAsync(brokerUrl !== undefined ? brokerUrl : 'mqtt://localhost:1833', ...rest);
   });
 }
 
 declare module '@mojojs/core/lib/types' {
   interface MojoContext {
-    mqttClient: (brokerUrl?: string, opts?: IClientOptions, allowRetries?: boolean) => Promise<MqttClient>;
+    mqttClient: (...args: Parameters<typeof connectAsync> | []) => Promise<MqttClient>;
   }
 }
