cc3342e42f36cc02132a88ce3885262322104d15
Daniel Mantovani <dmanto@gmail.com>
2024-09-06 09:56:05 -0300

    improve type definitions

--- src/mojo-plugin-mqtt-helper.ts
+++ src/mojo-plugin-mqtt-helper.ts
@@ -1,19 +1,26 @@
 import type {MojoApp} from '@mojojs/core/lib/types';
-import {connectAsync, type MqttClient} from 'mqtt';
+import {connectAsync, type MqttClient, type IClientOptions} from 'mqtt';
+
+// type ConnectParameters = Parameters<typeof connectAsync>;
+type ConnectParameters = [brokerUrl?: string, opts?: IClientOptions, allowRetries?: boolean];
 
 /**
  * mqttPlugin.
  * @param app
  */
 export default function mqttPlugin(app: MojoApp) {
-  app.addHelper('mqttClient', (_ctx, ...args: Parameters<typeof connectAsync>) => {
-    const [brokerUrl, ...rest] = args;
-    return connectAsync(brokerUrl !== undefined ? brokerUrl : 'mqtt://localhost:1833', ...rest);
+  app.addHelper('mqttClient', (_ctx, ...args: ConnectParameters) => {
+    const [brokerUrl, opts, allowRetries] = args;
+    return connectAsync(
+      brokerUrl !== undefined ? brokerUrl : 'mqtt://localhost:1833',
+      opts !== undefined ? opts : {},
+      allowRetries !== undefined ? allowRetries : false
+    );
   });
 }
 
 declare module '@mojojs/core/lib/types' {
   interface MojoContext {
-    mqttClient: (...args: Parameters<typeof connectAsync> | []) => Promise<MqttClient>;
+    mqttClient: (...args: ConnectParameters) => Promise<MqttClient>;
   }
 }
--- test-d/mojo-plugin-mqtt-helper.test-d.ts
+++ test-d/mojo-plugin-mqtt-helper.test-d.ts
@@ -7,4 +7,5 @@ const app = mojo();
 app.plugin(mqttPlugin);
 const ctx = app.newMockContext();
 expectType<Promise<MqttClient>>(ctx.mqttClient());
+expectType<Promise<MqttClient>>(ctx.mqttClient('mqtt://test.mosquitto.org'));
 expectType<Promise<MqttClient>>(ctx.mqttClient('mqtt://test.mosquitto.org', {clientId: 'someClientId'}, true));
